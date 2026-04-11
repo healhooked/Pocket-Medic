dd2eb95f474a943596a3279ba844f59935e11580
Oliver <oliver@Olivers-MacBook-Air.local>
2026-04-11 20:11:13 +0930

    Quartz sync: Apr 11, 2026, 8:11 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Pocket Medic",
+pageTitle: "Pocket Medic",
+logoSrc: "/static/logo.png",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
