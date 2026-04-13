b8d1eeb56e461d694d8b8f7d628958b217f41e29
Oliver <oliver@Olivers-MacBook-Air.local>
2026-04-13 17:21:07 +0930

    Quartz sync: Apr 13, 2026, 5:21 PM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,14 +1,17 @@
-import { pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
+import { pathToRoot } from "../util/path"
 
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
+      <a href={baseDir}>
+        <img src="/Pocket-Medic/static/logo.png" style="width: 28px; height: 28px; vertical-align: middle; margin-right: 8px;" />
+        {title}
+      </a>
     </h2>
   )
 }
@@ -17,7 +20,6 @@ PageTitle.css = `
 .page-title {
   font-size: 1.75rem;
   margin: 0;
-  font-family: var(--titleFont);
 }
 `
 
