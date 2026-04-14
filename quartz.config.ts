6fa7b16ad31c686424a4e2cdda9ef5fdac4f5a73
Oliver <oliver@Mac.stjohnnt.local>
2026-04-14 10:50:30 +0930

    Quartz sync: Apr 14, 2026, 10:50 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "healhooked.github.io/Pocket-Medic",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
