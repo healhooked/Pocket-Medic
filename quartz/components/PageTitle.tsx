489ac89bb4a079d4fbea865279887052acb15f2a
Oliver <oliver@Olivers-MacBook-Air.local>
2026-04-21 12:21:53 +0930

    Quartz sync: Apr 21, 2026, 12:21 PM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -9,7 +9,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   return (
     <div class={classNames(displayClass, "page-title")}>
       <a href={baseDir}>
-        <img src="https://healhooked.github.io/Pocket-Medic/logo.png" style="width: 100px; height: 150px; display: block; margin-bottom: 8px;" />
+        <img src="https://healhooked.github.io/Pocket-Medic/logo.png" style="width: 150px; height: 225px; display: block; margin-bottom: 8px;" />
         <span>{title}</span>
       </a>
     </div>
